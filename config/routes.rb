cc3f9b41da19a34e47240b58a4a68ebec8e0e483
mwb86 <masonwbrown@gmail.com>
2017-01-06 13:39:30 -0500

    changed some css, pushing to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'posts#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :posts do
-  resources :comments
-end
+  # resources :users do
+  #   resources :gyms do
+  #     resources :posts do
+  #       resources :comments
+  #     end
+  #   end
+  # end
+
+  resources :posts do
+    resources :comments
+  end
+# end
 resources :gyms
-root 'posts#index'
+
 end
