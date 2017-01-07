5ffa04fdf715d02359b1cb00f5bf376b4684fc82
mwb86 <masonwbrown@gmail.com>
2017-01-07 17:51:19 -0500

    added will_paginate gem and set up posts to display 10 at a time

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show]
 def index
-  @posts = Post.all.order('created_at DESC')
+  @posts = Post.paginate(:page => params[:page], :per_page => 10)
+  # @posts = Post.all.order('created_at DESC')
 end
 def new
   @post = Post.new
