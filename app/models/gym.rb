2267a956780b02db6a620ac02f15c51266bdc44e
mwb86 <masonwbrown@gmail.com>
2017-01-09 09:25:28 -0500

    working on comments and gyms

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,4 +1,6 @@
 class Gym < ApplicationRecord
   has_many :posts
   has_many :users
+  validates :name, presence: true, length: { minimum: 5}
+  validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-# end
 resources :gyms
 
 end
